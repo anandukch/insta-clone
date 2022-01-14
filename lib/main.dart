96973d26c18e21fa4ebb625d406635f65d983d06
anandukch <anandukch@gmail.com>
2022-01-14 11:13:23 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/responsive/mobile_screen_layout.dart';
+import 'package:insta_clone/responsive/responsive_layout_screen.dart';
+import 'package:insta_clone/responsive/web_screen_layout.dart';
+import 'package:insta_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Insta clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor:mobileBackgroundColor,
+      ),
+      home: ResponsiveLayout(mobileScreenLayout:MobileScreenLayout() , webScreenLayout:WebScreenLayout())
+    );
+  }
+}
